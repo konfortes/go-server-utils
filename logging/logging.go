379f6b9258cc6261ba72707dd9a59c6e4675681c
konfortes <ronen.konfortes@riskified.com>
2020-04-18 16:50:20 +0300

    some logging improvements

--- logging/logging.go
+++ logging/logging.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -14,19 +15,28 @@ import (
 type correlationIDType int
 
 const (
-	requestIDKey correlationIDType = iota
+	requestIDKey     correlationIDType = iota
+	requestIDKeyName                   = "reqId"
 )
 
-var logger *zap.Logger
+// a fallback/root logger for events without context
+// it is a sugared zap logger but can be de-sugared by the app when performance is critical
+var logger *zap.SugaredLogger
 
 func init() {
-	// a fallback/root logger for events without context
-	// TODO: handle error
-	if os.Getenv("GO_ENV") == "production" {
-		logger, _ = zap.NewProduction()
+	var l *zap.Logger
+	var err error
+
+	if os.Getenv("ENV") == "production" {
+		l, err = zap.NewProduction()
 	} else {
-		logger, _ = zap.NewDevelopment()
+		l, err = zap.NewDevelopment()
 	}
+
+	if err != nil {
+		log.Panic(err)
+	}
+	logger = l.Sugar()
 }
 
 // WithRqID returns a context contains a request ID
@@ -35,11 +45,11 @@ func WithRqID(ctx context.Context, requestID string) context.Context {
 }
 
 // Logger returns a zap logger with request id context if available
-func Logger(ctx context.Context) *zap.Logger {
+func Logger(ctx context.Context) *zap.SugaredLogger {
 	newLogger := logger
 	if ctx != nil {
 		if ctxRqID, ok := ctx.Value(requestIDKey).(string); ok {
-			newLogger = newLogger.With(zap.String("rqId", ctxRqID))
+			newLogger = newLogger.With(zap.String(requestIDKeyName, ctxRqID))
 		}
 	}
 	return newLogger
@@ -51,25 +61,61 @@ func RequestIDMiddleware() gin.HandlerFunc {
 		requestID, _ := uuid.NewRandom()
 		reqIDCtx := context.WithValue(c.Request.Context(), requestIDKey, requestID.String())
 		c.Request = c.Request.WithContext(reqIDCtx)
+
+		if c.Keys == nil {
+			c.Keys = make(map[string]interface{})
+		}
+		c.Keys[requestIDKeyName] = requestID.String()
+
 		c.Next()
 	}
 }
 
-// JSONLogMiddleware returns a gin HandlerFunc that logs json
+// RequestMiddleware returns a middleware to log requests in a json format
+// func RequestMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		start := time.Now()
+// 		c.Next()
+// 		timeStamp := time.Now()
+// 		latency := timeStamp.Sub(start)
+
+// 		// errorMessage := c.Errors.ByType(ErrorTypePrivate).String()
+
+// 		logger := Logger(c.Request.Context())
+// 		defer logger.Sync()
+
+// 		logger.With(
+// 			"ts", timeStamp,
+// 			"method", c.Request.Method,
+// 			"path", c.Request.URL.Path,
+// 			"duration", latency,
+// 			"statusCode", c.Writer.Status(),
+// 		).Info("got request")
+// 	}
+// }
+
+// JSONLogMiddleware returns a gin HandlerFunc that logs json using gin's infrastructure
 func JSONLogMiddleware() gin.HandlerFunc {
 	return gin.LoggerWithFormatter(formatter)
 }
 
 func formatter(params gin.LogFormatterParams) string {
-	var errorMessage string
+	var errorMessageEntry string
 	if len(params.ErrorMessage) > 0 {
-		errorMessage = fmt.Sprintf(`"error":"%s"`, params.ErrorMessage)
+		errorMessageEntry = fmt.Sprintf(`,"error":"%s"`, params.ErrorMessage)
 	}
-	return fmt.Sprintf(`{"timestamp":"%s","method":"%s","path":"%s","code":%d,"took":%d%s}`,
+
+	var reqIDEntry string
+	if id, found := params.Keys[requestIDKeyName].(string); found {
+		reqIDEntry = fmt.Sprintf(`,"%s":%s`, requestIDKeyName, id)
+	}
+
+	return fmt.Sprintf(`{"ts":"%s","method":"%s","path":"%s","code":%d,"duration":%d%s%s}`,
 		params.TimeStamp,
 		params.Method,
 		params.Path,
 		params.StatusCode,
 		params.Latency,
-		errorMessage) + "\n"
+		reqIDEntry,
+		errorMessageEntry) + "\n"
 }
